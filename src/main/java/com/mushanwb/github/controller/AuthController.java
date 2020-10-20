f1864f5cb59a9b167fd48bf6c3fb39bf459b8062
wangbo <1216476406@qq.com>
2020-10-20 17:07:57 +0800

    使用 web security 包进行假数据模拟用户登录操作
    在这个提交中，我先用写死的假数据来完成用户登录操作，后面会替换成数据库操作

--- src/main/java/com/mushanwb/github/controller/AuthController.java
+++ src/main/java/com/mushanwb/github/controller/AuthController.java
@@ -1,28 +1,98 @@
 package com.mushanwb.github.controller;
 
+import com.mushanwb.github.entity.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.Map;
 
 @Controller
 public class AuthController {
 
-    @GetMapping("/auth")
-    @ResponseBody
-    public Object auth(ModelMap map) {
-        return new Result();
+    private UserDetailsService userDetailsService;
+    private AuthenticationManager authenticationManager;
+
+    @Inject
+    public AuthController(UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
     }
 
+    @GetMapping("/auth")
+    @ResponseBody
+    public Result auth() {
+        return new Result("ok", "用户没有登录", false);
+    }
+
+    // 接口返回格式
     private static class Result {
+        String status;
+        String msg;
+        boolean isLogin;
+        Object data;
+
+        public Result(String status, String msg, boolean isLogin) {
+            this(status, msg, isLogin, null);
+        }
+
+        public Result(String status, String msg, boolean isLogin, Object data) {
+            this.status = status;
+            this.msg = msg;
+            this.isLogin = isLogin;
+            this.data = data;
+        }
+
         public String getStatus() {
-            return "ok";
+            return status;
         }
 
-        public boolean getIsLogin() {
-            return false;
+        public String getMsg() {
+            return msg;
         }
 
+        public boolean isLogin() {
+            return isLogin;
+        }
+
+        public Object getData() {
+            return data;
+        }
+    }
+
+    @PostMapping("/auth/login")
+    @ResponseBody
+    public Result login(@RequestBody Map<String, String> param) {
+        String username = param.get("username");
+        String password = param.get("password");
+
+        UserDetails userDetails = null;
+        try {
+             userDetails = userDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            return new Result("fail", "密码不正确", false);
+        }
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        try {
+            authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            User loginUser = new User(1, "张三");
+            return new Result("ok", "登录成功", true, loginUser);
+        } catch (BadCredentialsException e) {
+            return new Result("ok", "密码不正确", false);
+        }
     }
 
 }
