05a502215c5bdff60e3f613a703e66dd641d3960
mushan <49012328+mushanwb@users.noreply.github.com>
2020-10-21 09:52:33 +0800

    使用 map 结构存储用户信息 (#14)
    在这个提交中，我做了用户登录和登陆后获取用户信息的操作，
    但是使用的是 map 将用户信息进行存储，并且返回的是固定用户数据
    在下一个提交中，我将会把 map 替换成数据库

--- src/main/java/com/mushanwb/github/mapper/UserMapper.java
+++ src/main/java/com/mushanwb/github/mapper/UserMapper.java
@@ -9,4 +9,6 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("select * from user where id = #{id}")
     User findUserById(@Param("id") Integer id);
+
+
 }
--- src/main/java/com/mushanwb/github/service/UserService.java
+++ src/main/java/com/mushanwb/github/service/UserService.java
@@ -1,19 +1,46 @@
 package com.mushanwb.github.service;
 
 import com.mushanwb.github.entity.User;
-import com.mushanwb.github.mapper.UserMapper;
+import com.mysql.cj.xdevapi.Collection;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class UserService {
-    private UserMapper userMapper;
+public class UserService implements UserDetailsService {
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Map<String, String> userPasswords = new ConcurrentHashMap<>();
 
     @Inject
-    public UserService(UserMapper userMapper) {
-        this.userMapper = userMapper;
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        save("user", "password");
     }
 
-    public User getUserById(Integer id) {
-        return userMapper.findUserById(id);
+    public void save(String username, String password) {
+        userPasswords.put(username, bCryptPasswordEncoder.encode(password));
+    }
+
+    public String getPassword(String username) {
+        return userPasswords.get(username);
+    }
+
+    public User getUserByUsername(String username) {
+        return new User(1, username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!userPasswords.containsKey(username)) {
+            throw new UsernameNotFoundException(username + " 不存在");
+        }
+
+        String encodePassword = userPasswords.get(username);
+        return new org.springframework.security.core.userdetails.User(username, encodePassword, Collections.emptyList());
     }
 }
