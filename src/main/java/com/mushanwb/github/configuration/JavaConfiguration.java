129c0eb1ec6e03154d6365b5b482757eb7616adb
wangbo <1216476406@qq.com>
2020-10-22 11:58:16 +0800

    使用 Service 注解来申明 Bean

---
+++ src/main/java/com/mushanwb/github/configuration/JavaConfiguration.java
@@ -0,0 +1,18 @@
+package com.mushanwb.github.configuration;
+
+import com.mushanwb.github.mapper.UserMapper;
+import com.mushanwb.github.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class JavaConfiguration {
+
+//    @Bean
+//    public UserService userService(BCryptPasswordEncoder bCryptPasswordEncoder,
+//                                   UserMapper userMapper) {
+//        return new UserService(bCryptPasswordEncoder, userMapper);
+//    }
+
+}
--- src/main/java/com/mushanwb/github/service/UserService.java
+++ src/main/java/com/mushanwb/github/service/UserService.java
@@ -2,17 +2,16 @@ package com.mushanwb.github.service;
 
 import com.mushanwb.github.entity.User;
 import com.mushanwb.github.mapper.UserMapper;
-import com.mysql.cj.xdevapi.Collection;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
+@Service
 public class UserService implements UserDetailsService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private UserMapper userMapper;
